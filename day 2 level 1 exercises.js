4ef4ed5e67b1cbcb6d4a887ac051ab3f0408a798
olaolujnr <shololaolaoluwa@gmail.com>
2022-08-03 15:59:04 +0100

    day 2 level 1

---
+++ day 2 level 1 exercises.js
@@ -0,0 +1,25 @@
+/*Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript' */
+let challenge = "30 Days Of JavaScript"
+
+/*Print the string on the browser console using console.log() */
+console.log(challenge)
+
+/* Print the length of the string on the browser console using console.log()*/
+console.log(challenge.length)
+
+/* Change all the string characters to capital letters using toUpperCase() method*/
+console.log(challenge.toUpperCase())
+
+/*Change all the string characters to lowercase letters using toLowerCase() method */
+console.log(challenge.toLowerCase)
+
+/* Cut (slice) out the first word of the string using substr() or substring() method*/
+console.log(challenge.substring(3,7))
+
+/* Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript*/
+console.log(challenge.substring(3,))
+
+/*Check if the string contains a word Script using includes() method */
+console.log(challenge.includes(Script))
+
+/*plit the string into an array using split() method */
---
+++ solutions.js
@@ -0,0 +1,28 @@
+let challenge = "30 Days Of JavaScript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toLocaleLowerCase())
+console.log(challenge.substring(3,7))
+console.log(challenge.substring(3,))
+console.log(challenge.includes("Script"))
+console.log(challenge.split())
+console.log(challenge.split(" "))
+let list= 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(list.split(","))
+console.log(challenge.replace("JavaScript","Python"))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt(11))
+console.log(challenge.indexOf("a"))
+console.log(challenge.lastIndexOf("a"))
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.indexOf("because"))
+console.log(sentence.lastIndexOf("because"))
+console.log(sentence.search("because"))
+console.log(challenge.trim())
+console.log(challenge.startsWith(30))
+console.log(challenge.endsWith("Script"))
+console.log(challenge.match("a"))
+let a = '30 Days of'
+let b = 'JavaScript'
+console.log(a.concat(b))
+console.log(challenge.repeat(2))
